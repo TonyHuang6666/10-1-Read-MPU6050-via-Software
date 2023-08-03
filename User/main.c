47b956e3a80434a635a039b89604b4f7cdbc5d17
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-08-04 02:02:07 +0800

    高电平飞线接AD0后地址改为1101001，测试0xD2应答位为0，即成功！

--- User/main.c
+++ User/main.c
@@ -8,9 +8,14 @@ int main(void)
 	OLED_Init();
 	IIC_Init();
 	OLED_ShowString(1, 1, "HelloWorld!");
+	/*
+	以下三行相当于一个点名的时序，可以结合for循环遍历所有从机地址，
+	即前7位，把应答位为0的地址打印出来，实现扫描总线上设备的功能
+	*/
 	IIC_Start();
-	IIC_SendByte(0xA0);//1101000 0
+	IIC_SendByte(0xD2);//1101001 0
 	uint8_t ack = IIC_ReceiveACK();
+
 	IIC_Stop();
 	OLED_ShowNum(2, 1, ack, 3);
 	while (1)
