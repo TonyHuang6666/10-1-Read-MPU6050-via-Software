6c01ba4268ec82633df86b1955c4953a7731eea7
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-08-04 16:48:19 +0800

    再次验证读写功能都没问题

--- User/main.c
+++ User/main.c
@@ -9,7 +9,7 @@ int main(void)
 	MPU6050_Init();
 	OLED_ShowString(4, 16, "T");
 	MPU6050_WriteReg(0x6B, 0x00);//解除休眠状态
-	MPU6050_WriteReg(0x19, 0xAA);
+	MPU6050_WriteReg(0x19, 0x66);
 	uint8_t ID=MPU6050_ReadReg(0x19);
 	OLED_ShowHexNum(1, 1, ID, 2);
 	while (1)
