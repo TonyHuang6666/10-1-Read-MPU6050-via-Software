0f9035d90f216334f082d5b4f161072d8cfeb6ab
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-08-04 17:25:16 +0800

    MPU6050读传感器数据寄存器函数完成

--- Hardware/MPU6050.h
+++ Hardware/MPU6050.h
@@ -10,4 +10,6 @@
 uint8_t MPU6050_ReadReg(uint8_t reg);
 void MPU6050_WriteReg(uint8_t reg, uint8_t data);
 void MPU6050_Init(void);
+void MPU6050_GetData(int16_t *AccX,int16_t *AccY,int16_t *AccZ,
+                        int16_t *GyroX,int16_t *GyroY,int16_t *GyroZ);
 #endif
